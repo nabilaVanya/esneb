afc3bd083fa1d421987c81891e0299d8ae99de8f
nabilaVanya <nabila.vanya@student.alhazen.academy>
2025-05-16 08:48:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let angka1 = prompt("masukan angka pertama")
+let angka2 = prompt("masukan angka kedua")
+
+angka1 = parseFloat(angka1)
+angka2 = parsefloat(angka2)
+
+let hasil = angka1 + angka2
+alert("hasil penjumlahan" + hasil)
